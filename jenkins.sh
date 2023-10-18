f242a1524ddc232b4e0d378a7604ebcfa22909a2
vib2022 <106479001+vib2022@users.noreply.github.com>
2023-10-18 09:39:29 -0400

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -9,7 +9,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
 echo "Importing a key file from Jenkins-CI....."
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 sudo yum upgrade -y
 
